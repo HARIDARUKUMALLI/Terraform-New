8cc6b21047d41844b95652661b0360c12606224c
manasashetty15 <manasa.shetty15@gmail.com>
2019-12-02 16:08:56 +0530

    added .tf files

---
+++ main.tf
@@ -0,0 +1,51 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+resource "aws_security_group" "my-sg" {
+  name  = "Test-sg"
+  vpc_id  = "vpc-f76a358d"
+  ingress {
+  from_port = 22
+  to_port = 22
+  protocol  = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "ssh-sg"
+  }
+}
+
+output "security_group_id" {
+value  = "${aws_security_group.my-sg.id}"
+}
+
+resource "aws_instance" "example" {
+  ami  = "ami-04b9e92b5572fa0d1"
+  instance_type = "t2.micro"
+  associate_public_ip_address = "true"
+  count = "${var.instance_count}"
+  key_name  = "MyTest"
+  vpc_security_group_ids  = ["${aws_security_group.my-sg.id}"]
+
+tags  = {
+#  Name  = "example"
+#  Name = "${format("example-%03d", count.index + 1)}"
+  Name  = "${element(var.instance_tags, count.index)}"
+#  Name  = "{count.index}"
+}
+lifecycle {
+  create_before_destroy = "false"
+}
+}
+
+variable  "instance_count"  {
+  default = "2"
+}
+variable "instance_tags"  {
+  type  = "list"
+  default = [ "Test1", "Test2"]
+}
+
+output "instance_ips" {
+  value = ["${aws_instance.example.*.public_ip}"]
+}
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable  "instance_count"  {
+  default = "2"
+}
+variable "instance_tags"  {
+  type  = "list"
+  default = [ "Test1", "Test2"]
+}
