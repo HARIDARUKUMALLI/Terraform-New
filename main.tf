1c01dc3cd5e450fc883e18e12b5a2f0e1f1f2c88
Dileepkumar93 <57656132+Dileepkumar93@users.noreply.github.com>
2019-12-09 13:24:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "my-sg" {
 resource "aws_instance" "example" {
   ami  = "ami-0123b531fc646552f"
   instance_type = "t2.medium"
-#  associate_public_ip_address = "true"
+  associate_public_ip_address = "true"
   count = "${var.instance_count}"
   key_name  = "devops"
   vpc_security_group_ids  = ["${aws_security_group.my-sg.id}"]
@@ -36,31 +36,3 @@ lifecycle {
 }
 }
 
-resource "aws_instance" "test" {
-  ami  = "ami-0123b531fc646552f"
-  instance_type = "t2.medium"
- # associate_public_ip_address = "true"
-  count = "${var.instance_count}"
-  key_name  = "devops"
-  vpc_security_group_ids  = ["${aws_security_group.my-sg.id}"]
-
-tags  = {
- #Name  = "PROD"
-#  Name = "${format("example-%03d", count.index + 1)}"
- Name  = "${element(var.instance_tags, count.index)}"
-#  Name  = "{count.index}"
-}
-lifecycle {
-  create_before_destroy = "false"
-}
-}
-
-resource "aws_eip_association" "eip" {
-  instance_id = "${aws_instance.example[count.index]}"
-  allocation_id = "eipalloc-00496129794133ccb"
-}
-
-resource "aws_eip_association" "eip2" {
-  instance_id = "${aws_instance.test[count.index]}"
-  allocation_id = "eipalloc-0c86323a2fde68dfb"
-}
