89fc1a189d59f0cd61981e50a7648a27103aba27
Dileepkumar93 <57656132+Dileepkumar93@users.noreply.github.com>
2019-12-09 15:22:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,12 @@ resource "aws_security_group" "my-sg" {
   protocol  = "TCP"
   cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "ssh-sg"
   }
