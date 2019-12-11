ca336192aeccbe5d9d534de3dc377146fe041b2b
Dileepkumar93 <57656132+Dileepkumar93@users.noreply.github.com>
2019-12-11 12:43:54 +0530

    opened inbound rules to 8080

--- main.tf
+++ main.tf
@@ -10,6 +10,12 @@ resource "aws_security_group" "my-sg" {
   protocol  = "TCP"
   cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+  from_port = 8080
+  to_port = 8080
+  protocol  = "TCP"
+  cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port       = 0
     to_port         = 0
