8c7f0827fba56e3f06f1b4bdc21c78f8b5c3a736
gittadmin <58682443+gittadmin@users.noreply.github.com>
2019-12-16 12:44:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,5 +2,5 @@ output "instance_ips" {
   value = ["${aws_instance.example.*.public_ip}"]
 }
 output "security_group_id" {
-value  = "${aws_security_group.cicd-sg.id}"
+value  = "${aws_security_group.cicd-demo-sg.id}"
 }
